4e4ef3e1ba7c307946d6e211e4f3c378dea46df9
cpolitano <cpolitano8@gmail.com>
2015-05-16 19:08:09 -0400

    added event model validations and seed data

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ActiveRecord::Base
 
   has_and_belongs_to_many :tags
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :event_time, presence: true
+  validates :location, presence: true
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Event.create([
+  { 
+    name: "DjangoFest2015", 
+    description: "Getting djiggy with django!", 
+    event_time: "2015-08-02 09:00:00 EST", 
+    location: "iStrategyLabs",
+    link: "http://istrategylabs.com"
+  },
+  { 
+    name: "Women Who Code Hackathon", 
+    description: "Come hack on your code projects with cool female devs.", 
+    event_time: "2015-07-24 18:00:00 EST", 
+    location: "WeWork Shaw",
+    link: "http://wework.com"
+  },
+    { 
+    name: "Rails Hack Night", 
+    description: "Lightning talks featuring Rails projects, followed by work time. Beginners welcome!", 
+    event_time: "2015-07-07 19:30:00 EST", 
+    location: "iStrategyLabs",
+    link: "http://istrategylabs.com"
+  },
+    { 
+    name: "Code and Coffee", 
+    description: "BYO-Projects and laptops for a fun, informal way to learn, code and work on projects with other members in the community.", 
+    event_time: "2015-06-15 14:00:00 EST", 
+    location: "Compass Coffee",
+    link: "http://www.meetup.com/Women-Who-Code-DC/"
+  },
+])
