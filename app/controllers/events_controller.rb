893f07eb1e5411138678f966f77324cc3c072433
cpolitano <cpolitano8@gmail.com>
2015-05-17 11:07:01 -0400

    remove extraneous files, added more controller functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,47 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+      @event = Event.new
+  end
+
+  def create
+      @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else 
+      render 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+    redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :event_time, :location, :link)
+  end
+
 end
