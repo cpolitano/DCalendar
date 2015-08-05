211eb8c80d4762aaeca46430e368d9d1d82d163f
Clare Politano <clare@istrategylabs.com>
2015-08-05 13:52:03 -0400

    added more api endpoints and readme

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,45 @@
 class Api::EventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def index
     render json: Event.all
   end
+
+  def show
+    event = Event.find(params[:id])
+    render json: event
+  end
+
+  def create
+    event = Event.new(event_params)
+    if event.save
+      render json: {
+        status: 200,
+        message: "Successfully created event"
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    render json: {
+      status: 200,
+      message: "Successfully deleted event"
+    }.to_json
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :event_time, :location, :link)
+  end
+
 
 end
